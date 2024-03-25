da54ec4e08a372f693c2aa245f0eae58226782e6
Danish Qureshi <85394659+Danishqureshi8817@users.noreply.github.com>
2024-03-25 19:25:18 +0530

    crash done

--- src/index.tsx
+++ src/index.tsx
@@ -12,14 +12,38 @@ import { Provider } from 'react-redux'
 import { store } from './redux/store'
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
+import crashlytics from '@react-native-firebase/crashlytics';
 
 const persistor = persistStore(store);
 
 const App = () => {
 
+  const getUserDetails = async() => {
+    // console.log('gggg');
+    
+    crashlytics().log('User signed in.');
+    await Promise.all([
+      crashlytics().setUserId('99999'),
+      crashlytics().setAttribute('credits', String(999)),
+      crashlytics().setAttributes({
+        role: 'admin',
+        followers: '13',
+        email: 'phoenix@example.com',
+        username:'DANIITESSS',
+      }),
+    ]);
+  }
+
   useEffect(() => {
+    // crashlytics().crash()
+    crashlytics().log('Crashlytics Mounted')
+    getUserDetails()
     requestUserPermission()
     notificationListener()
+
+    return () => {
+      crashlytics().log('Crashlytics UnMounted')
+    }
 
   }, [])
 
